f1966a7e9dc7ca4cd424a0226f9a9bd69c8e5638
mahesh.chakkarwar <maheshchakkarwar21@gmail.com>
2023-03-23 10:27:43 +0530

    Navigation menu icon added.

--- app/src/main/java/com/example/ebilling/HomeActivity.kt
+++ app/src/main/java/com/example/ebilling/HomeActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.DrawerValue
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -22,6 +25,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.HomeScreen
 import com.example.NavDrawer
+import com.example.NavigationDrawer
 import com.example.ebilling.ui.DeleteProduct
 import com.example.ebilling.ui.EditProduct
 import com.example.ebilling.ui.RegisterProduct
@@ -39,13 +43,19 @@ class HomeActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     val navController = rememberNavController()
+                    val coroutineScope = rememberCoroutineScope()
+                    val scaffoldState =
+                        rememberScaffoldState(rememberDrawerState(DrawerValue.Closed));
+                    val navigationMenuClicked = remember {
+                        mutableStateOf("Home")
+                    }
                     NavHost(navController = navController, startDestination = "Register Product") {
                         composable("Register Product") { RegisterProduct() }
                         composable("Search Product") { SearchProduct() }
                         composable("Delete Product") { DeleteProduct() }
                         composable("Edit Product") { EditProduct() }
                     }
-                    HomeScreen(navController)
+                    HomeScreen(navController, scaffoldState, coroutineScope, navigationMenuClicked)
                 }
             }
         }
--- app/src/main/java/com/example/ebilling/ui/HomeScreen.kt
+++ app/src/main/java/com/example/ebilling/ui/HomeScreen.kt
@@ -1,62 +1,96 @@
 package com.example
 
+import android.view.View
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Divider
-import androidx.compose.material.DrawerValue
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.rememberDrawerState
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.R
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.ebilling.ui.theme.EBillingTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
-fun HomeScreen(navController: NavHostController) {
-    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
-    val coroutineScope = rememberCoroutineScope()
-    val navigationMenuClicked = remember {
-        mutableStateOf("Home")
-    }
+fun HomeScreen(
+    navController: NavHostController,
+    scaffoldState: ScaffoldState,
+    coroutineScope: CoroutineScope,
+    navigationMenuClicked: MutableState<String>
+) {
     EBillingTheme {
         Scaffold(
             Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(), topBar = {
+                .fillMaxHeight(),
+            topBar = {
                 TopAppBar(
-                    modifier = Modifier
-                        .wrapContentHeight()
-                        .fillMaxWidth()
-                ) {
-
-                }
-            }, scaffoldState = scaffoldState, drawerContent = {
+                    title = {
+                        Text(
+                            text = stringResource(id = com.example.ebilling.R.string.home_screen),
+                            modifier = Modifier.padding(25.dp),
+                            color = Color.White
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                               scaffoldState.drawerState.open()
+                            }
+                        }) {
+                            Icon(Icons.Filled.Menu, contentDescription = null)
+                        }
+                    }
+                )
+            },
+            scaffoldState = scaffoldState,
+            drawerContent = {
                 Text(
-                    stringResource(id = R.string.navigation_menu),
+                    stringResource(id = androidx.compose.ui.R.string.navigation_menu),
                     modifier = Modifier.padding(25.dp)
                 )
                 Divider()
-                NavDrawer(
+                NavigationDrawer(
                     stringArrayResource(id = com.example.ebilling.R.array.drawer_items),
                     scaffoldState,
                     coroutineScope,
                     navigationMenuClicked,
                     navController
                 )
+
             }) {
             Text(text = navigationMenuClicked.value, Modifier.padding(it))
         }
     }
+}
+
+@Composable
+fun NavigationDrawer(
+    drawerItems: Array<String>,
+    scaffoldState: ScaffoldState,
+    coroutineScope: CoroutineScope,
+    navigationMenuClicked: MutableState<String>,
+    navController: NavHostController
+) {
+    NavDrawer(
+        drawerItems,
+        scaffoldState,
+        coroutineScope,
+        navigationMenuClicked,
+        navController
+    )
 }
